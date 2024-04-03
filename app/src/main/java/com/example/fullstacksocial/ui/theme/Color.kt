a9f7eefe1bae973fdfc1e57e64323c837ce465f1
Shivam Sharma <shivamsharma2577@gmail.com>
2024-04-03 15:43:55 +0530

    initial project

---
+++ app/src/main/java/com/example/fullstacksocial/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.fullstacksocial.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF404040)
+val LightGray = Color(0xFF606060)
+val TextWhite = Color(0xFFEEEEEE)
+val HintGray = Color(0xFF6D6D6D)
+val BlueAccent = Color(0xFF13265C)
