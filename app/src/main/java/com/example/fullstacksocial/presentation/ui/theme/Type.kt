d0445a033e19c54ccfaf1d61b2b24ad365038c08
Shivam Sharma <shivamsharma2577@gmail.com>
2024-04-09 17:28:04 +0530

    UI material added

--- app/src/main/java/com/example/fullstacksocial/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/fullstacksocial/presentation/ui/theme/Type.kt
@@ -2,19 +2,47 @@ package com.example.fullstacksocial.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.fullstacksocial.R
+
+val quickSand = FontFamily(
+    Font(R.font.quicksand_bold, FontWeight.Bold),
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_medium, FontWeight.Medium),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_semibold, FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = quickSand,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        color = TextGray
+    ),
+    headlineLarge  = TextStyle(
+        fontFamily = quickSand,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        color = TextWhite
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = quickSand,
+        fontWeight = FontWeight.Medium,
+        fontSize = 24.sp,
+        color = TextWhite
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = quickSand,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        color = TextGray
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
