2b06938d0a4137bdc828159488478c8f58710aaa
Shivam Sharma <shivamsharma2577@gmail.com>
2024-04-09 17:38:36 +0530

    implemented standard text field composable

--- app/src/main/java/com/example/fullstacksocial/presentation/Components/StandardTextField.kt
+++ app/src/main/java/com/example/fullstacksocial/presentation/Components/StandardTextField.kt
@@ -1,2 +1,25 @@
 package com.example.fullstacksocial.presentation.Components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.constraintlayout.widget.Placeholder
+
+@Composable
+fun StandarTextField(
+    text : String,
+    hint : String,
+    onValueChanged: (String) -> Unit
+) {
+    TextField(
+        value = text,
+        onValueChange = onValueChanged,
+        placeholder = {
+            Text(text = hint, style = MaterialTheme.typography.bodyLarge)
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
