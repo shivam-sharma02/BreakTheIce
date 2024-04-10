5c31b247b6f553a869129267eab6b86edf010304
Shivam Sharma <shivamsharma2577@gmail.com>
2024-04-10 16:34:41 +0530

    implemented StandardTextField composable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     // Compose dependencies
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
-    implementation ("androidx.compose.material:material-icons-extended:1.6.4")
+    implementation ("androidx.compose.material:material-icons-extended:1.6.5")
     implementation ("com.google.accompanist:accompanist-flowlayout:0.17.0")
 
     // Coroutines
--- app/src/main/java/com/example/fullstacksocial/presentation/Components/StandardTextField.kt
+++ app/src/main/java/com/example/fullstacksocial/presentation/Components/StandardTextField.kt
@@ -1,24 +1,79 @@
 package com.example.fullstacksocial.presentation.Components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.constraintlayout.widget.Placeholder
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.example.fullstacksocial.R
 
 @Composable
 fun StandarTextField(
     text : String = "",
     hint : String = "",
+    isError: Boolean = false,
+    keyboardType: KeyboardType = KeyboardType.Text,
     onValueChanged: (String) -> Unit
 ) {
+
+    val isPasswordToggleDisplayed by remember {
+        mutableStateOf(keyboardType == KeyboardType.Password)
+    }
+
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+
     TextField(
         value = text,
         onValueChange = onValueChanged,
         placeholder = {
             Text(text = hint, style = MaterialTheme.typography.bodyLarge)
+        },
+        isError = isError,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        ),
+        visualTransformation = if (isPasswordToggleDisplayed){
+            PasswordVisualTransformation()
+        }else {
+            VisualTransformation.None
+              },
+        singleLine = true,
+        trailingIcon = {
+             if (isPasswordToggleDisplayed) {
+                 IconButton(onClick = {
+                     isPasswordVisible = !isPasswordVisible
+                 }) {
+                     Icon(
+                         imageVector = if (isPasswordVisible) {
+                             Icons.Filled.Visibility
+                         }else {
+                             Icons.Filled.VisibilityOff
+                         },
+                         contentDescription = if (isPasswordVisible) {
+                             stringResource(id = R.string.password_visible_content_description)
+                         }else{
+                             stringResource(id = R.string.password_hidden_content_description   )
+                         }
+                     )
+                 }
+             }
         },
         modifier = Modifier.fillMaxWidth()
     )
